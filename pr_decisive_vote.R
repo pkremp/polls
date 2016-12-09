3c7b9287cd83092ddb2d90f65006d853e626524d
pkremp <pkremp@gmail.com>
2016-12-09 09:29:37 +0100

    Adding analysis and data for Vox article on the electoral college magnifying the power of white voters (with A. Gelman)

--- pr_decisive_vote.R
+++ pr_decisive_vote.R
@@ -173,3 +173,73 @@ ggplot(data = p_votecounts %>% filter(state != "DC")) +
     scale_y_log10("Chance that your state is needed\nfor an electoral college win", breaks = .01*2^seq(0, 6), minor_breaks = NULL) +
     scale_color_gradientn("Pr(Clinton\nwins)",colors= c("red", "purple", "blue"), values = c(0, .5, 1), 
                           rescaler = function(x, ...) x, oob = identity)
+
+
+# For Vox article with Andrew:
+# Average probability of casting the decisive vote by ethnicity
+
+# Exit poll data on turnout by ethnicity
+turnout_ethnicity <- read.table("two_way_adjusted.txt", sep = " ", 
+                                header = TRUE, stringsAsFactors = FALSE)
+colnames(turnout_ethnicity) <- colnames(turnout_ethnicity) %>% tolower
+p_votecounts <- p_votecounts %>% left_join(turnout_ethnicity, by = "state") %>% filter(state != "DC")
+
+p_ethnicity <- data.frame(ethnicity = c("White", "Black", "Hispanic", "Other", "All"),
+           row = c(4, 3, 2, 1, NA),
+           pr = c(weighted.mean(p_votecounts$p_vote_decisive, p_votecounts$white),
+                 weighted.mean(p_votecounts$p_vote_decisive, p_votecounts$black),
+                 weighted.mean(p_votecounts$p_vote_decisive, p_votecounts$hispanic),
+                 weighted.mean(p_votecounts$p_vote_decisive, p_votecounts$other),
+                 weighted.mean(p_votecounts$p_vote_decisive, p_votecounts$voterturnout)
+                 ),
+           n = colSums(turnout_ethnicity[,c("white","black","hispanic","other", "voterturnout")]))
+
+p_ethnicity$pr_relative_to_whites <- p_ethnicity$pr / p_ethnicity$pr[1]
+p_ethnicity$pr_relative_to_all <- p_ethnicity$pr / p_ethnicity$pr[5]
+
+# Relative voting power (relative to the average voter)
+
+quartz(height = 4, width = 8)
+ggplot() +
+    geom_point(data = p_ethnicity[p_ethnicity$ethnicity != "All",], 
+               aes(x = pr_relative_to_all, y = row)) + 
+    geom_text(data = p_ethnicity[p_ethnicity$ethnicity != "All",], 
+              aes(pr_relative_to_all, row, label = paste(ethnicity, "voters")), hjust = c("left","right","right","right"), 
+              nudge_x  = c(1,-1,-1,-1)*.02, nudge_y = .02 ) +
+    geom_text(data = p_ethnicity[p_ethnicity$ethnicity == "All",], 
+              aes(pr_relative_to_all, 2.5, label = "Average Voter",angle = 270),  color = "red", size = 3.5, nudge_x = .01)+
+    ylim(0.5,4.5) +
+    scale_x_continuous(limits = c(.5, 1.2),
+                       breaks = b <- c(.5, .6, .7, .8, .9, 1, 1.1, 1.2),
+                       name = "Power of Voters by Ethnicity, Relative to the Average Voter",
+                       label = paste(ifelse(b != 1, paste(abs(1-b)*100, "%\n", sep = ""), "Same Power As\n"), 
+                                     ifelse(1-b > 0, "Less Power",
+                                            ifelse(b == 1, "Average US Voter",
+                                                   "More Power")), sep = "")
+                       ) +
+    geom_vline(xintercept = 1, color = "red") +
+    theme(axis.ticks.y = element_blank(), axis.text.y = element_blank(), axis.line.y = element_blank(), axis.title.y = element_blank(),  panel.grid.major.y = element_blank(), panel.grid.minor.y = element_blank(), panel.grid.minor.x = element_blank(), plot.margin = unit(c(1,1,1,1), "cm"))
+
+
+# secondary <- function(b) c(0, paste("1 in\n",round(1e-6/b[-1],1)," million", sep = ""))
+# 
+# quartz(height = 4, width = 8)
+# ggplot() +
+#     geom_point(data = p_ethnicity[p_ethnicity$ethnicity != "All",], 
+#                aes(x = pr, y = row)) + 
+#     geom_text(data = p_ethnicity[p_ethnicity$ethnicity != "All",], 
+#               aes(pr, row, label = paste(ethnicity, "voters")), hjust = c("left","right","right","right"), 
+#               nudge_x  = c(1,-1,-1,-1)*.25e-8, nudge_y = .02 ) +
+#     geom_text(data = p_ethnicity[p_ethnicity$ethnicity == "All",], 
+#               aes(pr, 2.5, label = "Average probability\nacross all ethnic groups",angle = 270),  color = "red", size = 3.5, nudge_x = 0)+
+#     ylim(0.5,4.5) +
+#     scale_x_continuous(breaks = b <- c(0, 2.5e-8, 5e-8, 7.5e-8, 10e-8, 12.5e-8), 
+#                        limits = c(0, 1.25e-7), expand = c(0,0),
+#                        name = "Probability that an individual voter would swing the election",
+#                        #label = c(expression(0), expression(2.5 %*% 10^-8), expression(5 %*% 10^-8), expression(7.5 %*% 10^-8), expression(10^-7), expression(1.25 %*% 10^-7)),
+#                        label = paste(c("0",sprintf("%.7f", b[-1]*100)),"%", sep = ""),
+#                        sec.axis = sec_axis(trans = ~ ., 
+#                                            breaks = c(0, 2.5e-8, 5e-8, 7.5e-8, 10e-8, 12.5e-8),
+#                                            labels = secondary)) +
+#     geom_vline(xintercept = p_ethnicity$pr[p_ethnicity$ethnicity == "All"], color = "red") +
+#     theme(axis.ticks.y = element_blank(), axis.text.y = element_blank(), axis.line.y = element_blank(), axis.title.y = element_blank(),  panel.grid.major.y = element_blank(), panel.grid.minor.y = element_blank(), panel.grid.minor.x = element_blank(), plot.margin = unit(c(1,1,1,1), "cm"))
